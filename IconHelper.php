e0e2285a4a780e4352b8affdfce4302884c61d1d
jeffrico <jeffrey@humanized.be>
2016-03-31 20:15:57 +0200

    Create IconHelper.php

---
+++ IconHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace \\humanized\\iconhelper\;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
